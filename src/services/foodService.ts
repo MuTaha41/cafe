e856bf2e4ec4feecb762eb795ce0d789023a851e
Basjabbie <100725442+Basjabbie@users.noreply.github.com>
2024-04-02 00:59:10 +0000

    3

--- src/services/foodService.ts
+++ src/services/foodService.ts
@@ -1,19 +1,18 @@
+// Importing faker
 import { faker } from "@faker-js/faker";
 
+// Assuming PizzaData is defined elsewhere in your project
+import { PizzaData } from './path_to_your_PizzaData_definition'; // Adjust the path as necessary
+
 export const generatePizzaData = (): PizzaData => {
-  // Generate random pizza data
   const generatedPizzaData: PizzaData = {
-    type: faker.random.arrayElement(["Margherita", "Pepperoni", "Hawaiian", "Veggie"]),
-    size: faker.random.arrayElement(["small", "medium", "large"]),
-    toppings: faker.random.arrayElements(
-      ["mushrooms", "peppers", "onions", "sausages", "bacon", "extra cheese"],
-      faker.datatype.number({ min: 1, max: 5 }) // Random number of toppings between 1 and 5
-    ),
-    price: faker.commerce.price(5, 20, 2, "$") // Random price between $5 and $20
+    type: faker.helpers.arrayElement(["Margherita", "Pepperoni", "Hawaiian", "Veggie"]),
+    size: faker.helpers.arrayElement(["small", "medium", "large"]),
+    toppings: faker.helpers.arrayElements([
+      "mushrooms", "peppers", "onions", "sausages", "bacon", "extra cheese"
+    ], faker.datatype.number({ min: 1, max: 5 })),
+    price: faker.commerce.price(5, 20, 2, "$")
   };
 
-  // Return pizza data
   return generatedPizzaData;
 };
-
-// If you need another type of food data generation function, you can create one in a similar manner.
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,12 +1,3 @@
-// interface PizzaData {
-//   type: string;
-//   size: string;
-//   toppings: string[];
-//   price: string;
-// }
-
-import { faker } from "@faker-js/faker";
-
 // Define the PizzaData interface
 interface PizzaData {
   type: string;        // e.g., "Margherita", "Pepperoni"
@@ -14,21 +5,4 @@ interface PizzaData {
   toppings: string[];  // e.g., ["mushrooms", "onions"]
   price: string;       // e.g., "$9.99"
 }
-
-// Function to generate random pizza data
-export const generatePizzaData = (): PizzaData => {
-  // Generate random pizza data
-  const generatedPizzaData: PizzaData = {
-    type: faker.random.arrayElement(["Margherita", "Pepperoni", "Hawaiian", "Veggie"]),
-    size: faker.random.arrayElement(["small", "medium", "large"]),
-    toppings: faker.random.arrayElements(
-      ["mushrooms", "peppers", "onions", "sausages", "bacon", "extra cheese"],
-      faker.datatype.number({ min: 1, max: 5 }) // Random number of toppings between 1 and 5
-    ),
-    price: faker.commerce.price(5, 20, 2, "$") // Random price between $5 and $20
-  };
-
-  // Return pizza data
-  return generatedPizzaData;
-};
 

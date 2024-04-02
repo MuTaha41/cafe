d49e79535d525944062d25452b5710478574a821
Basjabbie <100725442+Basjabbie@users.noreply.github.com>
2024-04-02 02:20:21 +0000

    4

--- src/services/foodService.ts
+++ src/services/foodService.ts
@@ -1,31 +1,29 @@
-// Importing faker
 import { faker } from "@faker-js/faker";
 
-// Assuming PizzaData is defined elsewhere in your project
-import { PizzaData } from './path_to_your_PizzaData_definition'; // Adjust the path as necessary
-
-// In your foodService.js file
-
-export function generateSushiData() {
-  // Example implementation
-  return {
-      sushiName: "Salmon Nigiri",
-      ingredients: ["Salmon", "Rice", "Wasabi"],
-      isVegetarian: false,
+export const generatePizzaFoodData = (): FoodData => {
+  // Generate random weather data
+  const generatedFoodData = {
+    city: "Pizza",
+    temperature: faker.number.int({ min: -15, max: 30 }),
+    humidity: faker.number.int({ min: 79, max: 86 }),
+    wind: faker.number.int({ min: 2, max: 78 }),
+    rain: faker.number.int({ min: 65, max: 75 }),
   };
-}
 
+  // Return weather data
+  return generatedFoodData;
+};
 
-export const generatePizzaData = (): PizzaData => {
-  const generatedPizzaData: PizzaData = {
-    type: faker.helpers.arrayElement(["Margherita", "Pepperoni", "Hawaiian", "Veggie"]),
-    size: faker.helpers.arrayElement(["small", "medium", "large"]),
-    toppings: faker.helpers.arrayElements([
-      "mushrooms", "peppers", "onions", "sausages", "bacon", "extra cheese"
-    ], faker.datatype.number({ min: 1, max: 5 })),
-    price: faker.commerce.price(5, 20, 2, "$")
+export const generateSuzziFoodData = (): FoodData => {
+  // Generate random weather data
+  const generatedFoodData: FoodData = {
+    city: "Suzzi",
+    temperature: faker.number.int({ min: -15, max: 30 }),
+    humidity: faker.number.int({ min: 79, max: 86 }),
+    wind: faker.number.int({ min: 2, max: 78 }),
+    rain: faker.number.int({ min: 65, max: 75 }),
   };
-  
 
-  return generatedPizzaData;
+  // Return weather data
+  return generatedFoodData;
 };

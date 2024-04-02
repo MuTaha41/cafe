6b7f04064e21d9a6320c348f571dd7a99ea52d5f
Basjabbie <100725442+Basjabbie@users.noreply.github.com>
2024-04-02 03:05:42 +0000

    2

--- src/controllers/foodController.ts
+++ src/controllers/foodController.ts
@@ -1,50 +1,95 @@
+// import { Request, Response } from "express";
+// import {
+//   generatePizzaFoodData,
+//   generateSuzziFoodData,
+// } from "../services/foodService.js";
+// import { validationResult } from "express-validator";
+
+// /**
+//  * Gets the food data for a specified food type
+//  * @param req the request object
+//  * @param res the response object
+//  */
+// export const getFoodData = async (req: Request, res: Response) => {
+//   // We will use the validationResult function to check if there are any validation errors
+//   const errors = validationResult(req);
+
+//   // If there are validation errors, we will log them and send a 400 status code
+//   if (!errors.isEmpty()) {
+//     console.error("Validation error", errors.mapped());
+//     res.status(400).json({ errors: errors.array() });
+//     return;
+//   }
+
+//   // We will use a try catch block to catch any errors
+//   try {
+//     // Get the foodType param from the request
+//     const { foodType } = req.params;
+//     console.log(foodType);
+
+//     // We will create a variable with a type of FoodData
+//     let finalFoodData: FoodData;
+
+//     // We will use an if statement to check which food was passed in
+//     if (foodType === "Pizza") {
+//       console.log(generatePizzaFoodData());
+//       finalFoodData = generatePizzaFoodData();
+//     } else if (foodType === "Suzzi") {
+//       finalFoodData = generateSuzziFoodData();
+//     } else {
+//       // If the food is not pizza or suzzi, we will throw an error
+//       res.status(404).send("${foodType} not found");
+//     }
+
+//     // We will return the food data as JSON
+//     res.status(200).json(finalFoodData);
+//   } catch (error) {
+//     // If there is an error, we will log it and send a 500 status code
+//     res.status(500).send("Error in fetching food data");
+//   }
+// };
+
+// foodController.js
+
 import { Request, Response } from "express";
-import {
-  generatePizzaFoodData,
-  generateSuzziFoodData,
-} from "../services/foodService.js";
 import { validationResult } from "express-validator";
+import { generatePizzaFoodData, generateSuzziFoodData } from "../services/foodService.js";
 
-/**
- * Gets the food data for a specified food type
- * @param req the request object
- * @param res the response object
- */
 export const getFoodData = async (req: Request, res: Response) => {
-  // We will use the validationResult function to check if there are any validation errors
   const errors = validationResult(req);
-
-  // If there are validation errors, we will log them and send a 400 status code
   if (!errors.isEmpty()) {
-    console.error("Validation error", errors.mapped());
-    res.status(400).json({ errors: errors.array() });
-    return;
+    return res.status(400).json({ errors: errors.array() });
   }
 
-  // We will use a try catch block to catch any errors
   try {
-    // Get the foodType param from the request
     const { foodType } = req.params;
-    console.log(foodType);
+    let finalFoodData;
 
-    // We will create a variable with a type of FoodData
-    let finalFoodData: FoodData;
-
-    // We will use an if statement to check which food was passed in
-    if (foodType === "Pizza") {
-      console.log(generatePizzaFoodData());
-      finalFoodData = generatePizzaFoodData();
-    } else if (foodType === "Suzzi") {
-      finalFoodData = generateSuzziFoodData();
-    } else {
-      // If the food is not pizza or suzzi, we will throw an error
-      res.status(404).send("${foodType} not found");
+    switch (foodType) {
+      case "Pizza":
+        finalFoodData = generatePizzaFoodData();
+        break;
+      case "Suzzi":
+        finalFoodData = generateSuzziFoodData();
+        break;
+      default:
+        return res.status(400).json({
+          errors: [
+            {
+              type: "field",
+              value: foodType,
+              msg: "Food type must be either Pizza or Suzzi",
+              path: "foodType",
+              location: "params"
+            }
+          ]
+        });
     }
 
-    // We will return the food data as JSON
     res.status(200).json(finalFoodData);
   } catch (error) {
-    // If there is an error, we will log it and send a 500 status code
+    console.error("Error in fetching food data", error);
     res.status(500).send("Error in fetching food data");
   }
 };
+

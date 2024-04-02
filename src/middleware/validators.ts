89951f6a11502828c47ef94bd76fe0227e2f1505
Basjabbie <100725442+Basjabbie@users.noreply.github.com>
2024-04-02 14:25:17 +0000

    2

--- src/middleware/validators.ts
+++ src/middleware/validators.ts
@@ -1,19 +1,26 @@
+// import { param } from "express-validator";
+
+// /**
+//  * Validates the foodType name param
+//  * @returns {ValidationChain} - Express validator validation chain
+//  * @example
+//  * router.get(
+//  *  "/:foodType",
+//  * validateFoodTypeName,
+//  * getFoodData
+//  * );
+//  */
+// export const validateFoodName = param("foodType")
+//   // We will use the isString method to check if the foodType param is a string
+//   .isString()
+//   // We will use the isIn method to check if the foodType param is either pizza or suzzi
+//   .isIn(["pizza", "suzzi"])
+//   // We will use the withMessage method to set a custom error message
+//   .withMessage("foodType name must be either pizza or suzzi");
+
 import { param } from "express-validator";
 
-/**
- * Validates the foodType name param
- * @returns {ValidationChain} - Express validator validation chain
- * @example
- * router.get(
- *  "/:foodType",
- * validateFoodTypeName,
- * getFoodData
- * );
- */
 export const validateFoodName = param("foodType")
-  // We will use the isString method to check if the foodType param is a string
   .isString()
-  // We will use the isIn method to check if the foodType param is either pizza or suzzi
-  .isIn(["pizza", "suzzi"])
-  // We will use the withMessage method to set a custom error message
-  .withMessage("foodType name must be either pizza or suzzi");
+  .custom((value) => ["pizza", "suzzi"].includes(value.toLowerCase()))
+  .withMessage("Food type must be either pizza or suzzi");

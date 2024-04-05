d388c62c53a9d24debd28b882e0fb8c9b7bcf9ca
Basjabbie <100725442+Basjabbie@users.noreply.github.com>
2024-04-05 14:51:31 +0000

    2#

--- src/routes/foodRoute.ts
+++ src/routes/foodRoute.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import { getFoodData } from "../controllers/foodController.js";
-import { validateFoodName } from "../middleware/validators.js";
+import { validatefoodTypeName } from "../middleware/validators.js";
 
 // We will create a router object
 const router = express.Router();
 
 // We will create a route for the food data based on the foodType name
-router.get("/:foodType", validateFoodName, getFoodData);
+router.get("/:foodType", validatefoodTypeName, getFoodData);
 
 
 // We will export the router

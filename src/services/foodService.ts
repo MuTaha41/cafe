8b286a01ffb347841918d249919b00136524a276
Basjabbie <100725442+Basjabbie@users.noreply.github.com>
2024-04-13 23:11:08 +0000

    1

--- src/services/foodService.ts
+++ src/services/foodService.ts
@@ -1,46 +1,8 @@
-// import { faker } from "@faker-js/faker";
-// import { storeFoodData } from "../helpers/helpers.js";
-
-// const validCategories = ['Vegetarian', 'Vegan', 'Seafood', 'Regular'];
-
-// export const generateFoodData = (type: string, category: string): FoodData => {
-//   if (!validCategories.includes(category)) {
-//     throw new Error(`Invalid category: ${category}. Must be one of ${validCategories.join(", ")}`);
-//   }
-
-//   const baseToppings = {
-//     Vegetarian: ["bell peppers", "spinach", "mushrooms", "olives", "tomatoes"],
-//     Vegan: ["tomatoes", "lettuce", "cucumbers", "bell peppers", "chickpeas"],
-//     Seafood: ["shrimp", "tuna", "salmon", "squid", "anchovies"],
-//     Regular: ["cheese", "pepperoni", "ham", "bacon", "sausage"]
-//   };
-
-//   const generatedFoodData: FoodData = {
-//     type: type,
-//     size: faker.helpers.arrayElement(["small", "medium", "large"]),
-//     toppings: faker.helpers.arrayElements(baseToppings[category], faker.datatype.number({ min: 1, max: 5 })),
-//     price: faker.commerce.price(5, 30, 2, "$"),
-//     calories: faker.datatype.number({ min: 250, max: 1200 }),
-//     category: category as 'Vegetarian' | 'Vegan' | 'Seafood' | 'Regular'
-//   };
-
-//   storeFoodData(generatedFoodData).catch(console.error);
-//   return generatedFoodData;
-// };
-
 import { faker } from "@faker-js/faker";
 import { storeFoodData } from "../helpers/helpers.js";
 
 type Category = 'Vegetarian' | 'Vegan' | 'Seafood' | 'Regular';
 
-// interface FoodData {
-//   foodType: string;
-//   size: string;
-//   toppings: string[];
-//   price: string;
-//   calories: number;
-//   category: Category;
-// }
 
 const validCategories: Category[] = ['Vegetarian', 'Vegan', 'Seafood', 'Regular'];
 
--- src/types/global.d..ts
+++ src/types/global.d..ts
@@ -1,12 +1,3 @@
-// interface FoodData {
-//   foodType: string;
-//   size: string;
-//   toppings: string[];
-//   price: string;
-//   calories: number;
-//   category: 'Vegetarian' | 'Vegan' | 'Seafood' | 'Regular';
-// }
-// In global.d.ts
 interface FoodData {
   foodType: string;
   size: string;

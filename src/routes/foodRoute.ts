b13cd6b1820018a60d6d87529449676e0e75ea90
Basjabbie <100725442+Basjabbie@users.noreply.github.com>
2024-04-18 22:37:14 +0000

    n

--- src/routes/foodRoute.ts
+++ src/routes/foodRoute.ts
@@ -1,16 +1,3 @@
-// import express from "express";
-// import { getFoodData } from "../controllers/foodController.js";
-// import { validatefoodTypeName } from "../middleware/validators.js";
-
-// // We will create a router object
-// const router = express.Router();
-
-// // We will create a route for the food data based on the foodType name
-// router.get("/:foodType", validatefoodTypeName, getFoodData);
-
-
-// // We will export the router
-// export default router;
 import express from "express";
 import { getFoodData } from "../controllers/foodController.js";
 import { validateFoodTypeName } from "../middleware/validators.js";  // Corrected the function name here

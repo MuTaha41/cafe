636c0c2feaa9b4255a97a6e838c607ae1e3ffcb8
MuTaha41 <125828204+MuTaha41@users.noreply.github.com>
2024-04-30 13:27:55 +0000

    change

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,43 @@
 import express from "express";
-import drinkRoute from "./routes/drinkRoute.js"; // Updated import to handle drink routes
+import drinkRoute from "./routes/drinkRoute.js";
 import cors from "cors";
-
+ 
 const app = express();
 const PORT = 3000;
-
 app.use(express.json());
 app.use(cors());
-
-// Updated the API route to reflect drinks
 app.use("/api/drink", drinkRoute);
-
-// Error handling middleware should be the last piece of middleware added to the app
-app.use((err, _req, res, next) => {
-  if (!err) return next(); // if there is no error, continue to the next middleware
-  // Handle the error and send a response
-  res.status(err.status || 500).json({
-    error: {
-      message: err.message,
-    },
-  });
-});
-
-// Start the express server
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+    console.log(`Server is running at ${PORT}`);
 });
+
+
+
+// import express from "express";
+// import drinkRoute from "./routes/drinkRoute.js"; // Updated import to handle drink routes
+// import cors from "cors";
+
+// const app = express();
+// const PORT = 3000;
+
+// app.use(express.json());
+// app.use(cors());
+
+// // Updated the API route to reflect drinks
+// app.use("/api/drink", drinkRoute);
+
+// // Error handling middleware should be the last piece of middleware added to the app
+// app.use((err, _req, res, next) => {
+//   if (!err) return next(); // if there is no error, continue to the next middleware
+//   // Handle the error and send a response
+//   res.status(err.status || 500).json({
+//     error: {
+//       message: err.message,
+//     },
+//   });
+// });
+
+// // Start the express server
+// app.listen(PORT, () => {
+//   console.log(`Server is listening on port ${PORT}`);
+// });

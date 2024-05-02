af15ff5ccd65d0124264f5a7e1837e17befefba6
MuTaha41 <125828204+MuTaha41@users.noreply.github.com>
2024-05-02 11:36:53 +0000

    change

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,7 @@ const algodToken = "a".repeat(64);
 const server: string = "http://localhost";
 const port: string = "4001";
 
-const mnemonic: string = "rescue stereo cry taste scrub matter dress mesh interest live turtle cat rate used gate pioneer sail supreme earn unfair fork sphere kiwi absorb subway";
+const mnemonic: string = "merge thing later ankle vibrant fiber sausage humor oven february mutual clarify crazy giggle huge lunar betray area response stamp need flip broken ability shove";
 // "canoe mule cart impulse debris bachelor pelican case rocket apart donkey blanket loan grief upper lake pyramid turtle amused amateur alcohol safe armor abandon lumber";
 
 export function getClient(): algosdk.Algodv2 {
